4699eaffe473cc0fbf1cbcc0589ef7f51fa92a01
Russell Carlin <recarlin@fullsail.com>
2012-08-29 21:15:29 -0400

    Creating repository.

---
+++ Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Russell Carlin on 8/29/12.
+//  Copyright (c) 2012 Russell Carlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project1Tests/Project1Tests.h
@@ -0,0 +1,13 @@
+//
+//  Project1Tests.h
+//  Project1Tests
+//
+//  Created by Russell Carlin on 8/29/12.
+//  Copyright (c) 2012 Russell Carlin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Project1Tests : SenTestCase
+
+@end
